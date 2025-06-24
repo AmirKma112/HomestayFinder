8c1313a638f303cab54d024ee7b34facfd8512c3
amirbuk <amirbukhari346@gmail.com>
2025-06-24 23:52:54 +0800

    Update login.js

--- web/js/login.js
+++ web/js/login.js
@@ -4,24 +4,18 @@ $(document).ready(function () {
   const login = (p.get("login") || "").trim();
   const role = (p.get("role") || "").trim();
 
-  console.log("Debug:", { success, login, role });
-
-  // Simpan role dalam sessionStorage jika login berjaya
   if (login === "success" && role) {
     sessionStorage.setItem("user_role", role);
   }
 
-  // ✅ Popup jika daftar berjaya
   if (success === "1") {
     showPopup("Registration successful! Please log in.");
   }
 
-  // ✅ Popup jika login gagal
   if (login === "fail") {
     showPopup("Incorrect email or password!");
   }
 
-  // ✅ Popup jika login berjaya
   if (login === "success") {
     if (role === "customer") {
       showPopup("You are logged in as a Customer.");
@@ -30,28 +24,19 @@ $(document).ready(function () {
     } else {
       showPopup("Successful Login.");
     }
+
+    setTimeout(() => {
+      const userRole = sessionStorage.getItem("user_role");
+      if(userRole === "homestay_owner"){
+          window.location.replace("OwnerDashboardServlet");
+      } else if (userRole === "customer"){
+          window.location.replace("CustomerDashboardServlet");
+      } else {
+          window.location.replace("homepage.jsp");
+      }
+    }, 2000);
   }
 
-  // ✅ Bila popup ditutup → redirect ikut jenis user
-  // login.js
-    $("#popupClose").click(function () {
-      $("#popupBox").fadeOut();
-
-      if (login === "success"){
-        const userRole = sessionStorage.getItem("user_role");
-
-        if(userRole === "homestay_owner"){
-            window.location.replace("OwnerDashboardServlet");
-        } else if (userRole === "customer"){
-            window.location.replace("CustomerDashboardServlet");
-        } else {
-            window.location.replace("homepage.jsp");
-        }
-      }
-    });
-
-
-  // Fungsi paparan popup
   function showPopup(msg) {
     $("#popupMessage").text(msg);
     $("#popupBox").fadeIn();
